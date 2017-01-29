523bdff38f580d2bdb034cb0ee42c5b23c24a114
Dylan Chong <dylan.chong@icloud.com>
2017-01-30 08:09:56 +1300

    WIP started adding registry and tests

---
+++ lib/kv/registry.ex
@@ -0,0 +1,10 @@
+defmodule KV.Registry do
+  use GenServer
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok)
+  end
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+end
---
+++ test/kv/registry_test.exs
@@ -0,0 +1,7 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+
+  test "start_link/0 returns {:ok, pid}" do
+    assert {:ok, pid} = KV.Registry.start_link()
+  end
+end
