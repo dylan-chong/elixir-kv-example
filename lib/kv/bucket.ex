3deefdca7c9f8dbab195ec47be9867dec3387db3
Dylan Chong <dylan.chong@icloud.com>
2017-02-06 20:04:24 +1300

    Added specs to make warnings go away

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -1,23 +1,29 @@
 defmodule KV.Bucket do
+  @moduledoc """
+  For storing state in a map
+  """
+
+  @spec start_link :: {:ok, pid}
   def start_link do
     start_link(%{})
   end
 
+  @spec start_link(map) :: {:ok, pid}
   def start_link(map) do
     Agent.start_link fn -> map end
   end
 
+  @spec get(pid, any) :: any | nil
   def get(pid, key) do
     Agent.get(pid, &Map.get(&1, key))
   end
 
-  @doc """
-  Returns :ok
-  """
+  @spec put(pid, any, any) :: :ok
   def put(pid, key, value) do
     Agent.update(pid, &Map.put(&1, key, value))
   end
 
+  @spec delete(pid, any) :: any | nil
   def delete(pid, key) do
     Agent.get_and_update(pid, &Map.pop(&1, key))
   end
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,7 +1,11 @@
 defmodule KV.RegistryTest do
+  @moduledoc """
+  """
+
   use ExUnit.Case, async: true
 
   test "start_link/0 returns {:ok, pid}" do
     assert {:ok, pid} = KV.Registry.start_link()
   end
+
 end
