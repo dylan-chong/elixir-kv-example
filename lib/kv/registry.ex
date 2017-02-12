a767dea1729f5e436f227f37e81f1267cb9e9e53
Dylan Chong <dylan.chong@icloud.com>
2017-02-12 15:03:14 +1300

    Finished KV.Registry and KV.RegistryTest

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,10 +1,63 @@
 defmodule KV.Registry do
+  @moduledoc """
+  For registering processes with strings instead of atoms
+  """
+
   use GenServer
+
+  ## Client methods
+
+  @spec start_link :: {:ok, pid}
   def start_link do
     GenServer.start_link(__MODULE__, :ok)
   end
 
+  @spec start_link(map) :: {:ok, pid}
+  def start_link(name_to_bucket_map) do
+    GenServer.start_link(__MODULE__, name_to_bucket_map)
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`
+  Can return `:error`
+  """
+  @spec lookup(pid, String.t) :: {:ok, pid} | :error
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Ensures there is a bucket associated to the given `name` in `server`
+  """
+  @spec create(pid, String.t) :: any
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  ## Server callbacks
+
+  @spec init(:ok) :: {:ok, %{}}
   def init(:ok) do
-    {:ok, %{}}
+    {:ok, %{}} # map of names
+  end
+
+  @spec init(map) :: {:ok, map}
+  def init(name_to_bucket_map) do
+    {:ok, name_to_bucket_map}
+  end
+
+  @spec handle_call({:lookup, String.t}, none, map) :: {:reply, any} # TODO
+  def handle_call({:lookup, name}, _from, name_to_bucket_map) do
+    {:reply, Map.fetch(name_to_bucket_map, name), name_to_bucket_map}
+  end
+
+  @spec handle_cast({:create, String.t}, map) :: {:noreply, map}
+  def handle_cast({:create, name}, name_to_bucket_map) do
+    if Map.has_key?(name_to_bucket_map, name) do
+      {:noreply, name_to_bucket_map}
+    end
+
+    {:ok, bucket} = KV.Bucket.start_link
+    {:noreply, Map.put(name_to_bucket_map, name, bucket)}
   end
 end
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,11 +1,31 @@
 defmodule KV.RegistryTest do
   @moduledoc """
   """
-
   use ExUnit.Case, async: true
+  require IEx
 
-  test "start_link/0 returns {:ok, pid}" do
-    assert {:ok, pid} = KV.Registry.start_link()
+  setup do
+    {:ok, registry} = KV.Registry.start_link()
+    {:ok, registry: registry}
+  end
+
+  test "lookup/2 should return `:error` for non-existent bucket ",
+  %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "create/2 should create a KV.Bucket", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, _bucket} = KV.Registry.lookup(registry, "shopping")
+  end
+
+  test "lookup/2 should return {:ok, pid} for an existing bucket" do
+    created_bucket = KV.Bucket.start_link()
+
+    {:ok, registry} = KV.Registry.start_link(%{"shopping" => created_bucket})
+
+    {:ok, looked_up_bucket} = KV.Registry.lookup(registry, "shopping")
+    assert created_bucket == looked_up_bucket
   end
 
 end
